4cfd1e7e763a30ba458acd203bccb5d7aa99a25a
Vu Hong Minh <25021895@vnu.edu.vn>
2026-05-11 03:57:04 +0700

    check1

--- client/src/main/java/AuctionApp.java
+++ client/src/main/java/AuctionApp.java
@@ -1,2 +1,22 @@
-public class AuctionApp {
+import StageManager.SceneManager;
+import StageManager.SceneName;
+import StageManager.SwitchSceneControll;
+import javafx.application.Application;
+
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class AuctionApp extends Application {
+    @Override
+    public void start(Stage mainstage) throws Exception {
+        SceneManager.setStage(mainstage);
+        Scene scene = SwitchSceneControll.loadScene(SceneName.REGISTER);
+        mainstage.setScene(scene);
+        mainstage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
 }
