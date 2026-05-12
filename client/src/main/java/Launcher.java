b1ef94b4bf1a3640be0908be3c07a81727556dc5
laptq1909 <25021843@vnu.edu.vn>
2026-05-12 21:37:42 +0700

    Delete client directory

--- client/src/main/java/Launcher.java
+++
@@ -1,9 +0,0 @@
-package View;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(SignUpApplication.class, args);
-    }
-}
