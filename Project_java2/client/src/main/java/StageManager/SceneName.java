4092484ed496ba13ebefb1db49edad13fdc0cd78
laptq1909 <25021843@vnu.edu.vn>
2026-06-01 16:17:03 +0700

    Delete Project_java2 directory

--- Project_java2/client/src/main/java/StageManager/SceneName.java
+++
@@ -1,8 +0,0 @@
-package StageManager;
-
-public enum SceneName {
-    LOGIN,
-    REGISTER,
-    AUCTION_LIST,
-    BID
-}
--- Project_java2/server/src/main/java/Model/AuctionStatus.java
+++
@@ -1,9 +0,0 @@
-package Model;
-
-public enum AuctionStatus {
-    OPEN,
-    RUNNING,
-    FINISHED,
-    PAID,
-    CANCELED
-}
