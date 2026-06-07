442dc227fd9b984a1a8623cd0b9d71e2c8b7c1ac
Vu Hong Minh <25021895@vnu.edu.vn>
2026-06-07 22:25:47 +0700

    check2

--- Server/src/main/java/Server/Controller/WalletController.java
+++ Server/src/main/java/Server/Controller/WalletController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("wallets")
+@RequestMapping("/wallets")
 public class WalletController {
     private final WalletService walletService;
 
